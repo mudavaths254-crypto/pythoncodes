7631b92c65d566992d066721fbd06758df14dff2
mudavaths254-crypto <mudavaths254@gmail.com>
2025-09-25 14:55:58 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,21 @@
+print("Hello")
+print("hello")
+print("there")
+
+
+num = int(input("Enter number: "))
+
+if num%2==0:
+    print("even")
+else:
+     print("odd")       
+
+
+id_card = "present"
+
+
+if id_card=="present":
+   print("Allow student")
+else:
+    print("Bunk")
+ 
